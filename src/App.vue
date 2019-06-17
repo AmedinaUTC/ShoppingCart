6f2ac2a5ddab50bfe676859a8f6a0b6157ee83d4
alfredo <alfredo.medina@utculiacan.edu.mx>
2019-06-17 07:30:01 -0600

    Segunda Clase

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,31 @@
 <template>
     <div class="container">
-        <div class="row">
-            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Vuex</h1>
-                <app-result :counter="counter"></app-result>
-                <hr>
-                <app-counter @updated="counter += $event"></app-counter>
-            </div>
-        </div>
+       
+       <div class="row">
+
+           <Products
+                v-for="product in forSale" 
+                :key="product.invId"
+                :invId="product.invId"
+                :name ="product.name"
+                :image="product.image"
+                :price="product.price"
+            />
+
+       </div>
     </div>
 </template>
 
 <script>
-    import Counter from './components/Counter.vue';
-    import Result from './components/Result.vue';
+   
+    import Products from './components/Products.vue'
 
     export default {
-        data() {
-            return {
-                counter: 0
-            }
+        computed:{
+            forSale() {return this.$store.getters.forSale}
         },
         components: {
-            appCounter: Counter,
-            appResult: Result,
+            Products: Products,        
         }
     }
 </script>
